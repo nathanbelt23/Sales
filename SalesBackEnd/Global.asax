26b3b12acb258152cfc17df910c8319e0213491c
nathanbelt23 <40368449+nathanbelt23@users.noreply.github.com>
2020-12-17 18:08:39 -0500

    Project Creation
    Yonathan

---
+++ SalesBackEnd/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SalesBackEnd.MvcApplication" Language="C#" %>
---
+++ SalesBackEnd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SalesBackEnd.Startup))]
+namespace SalesBackEnd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
