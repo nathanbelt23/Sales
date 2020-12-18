3e7af5fd6946a2863806ab747cb8e124e3a4c6cf
nathanbelt23 <40368449+nathanbelt23@users.noreply.github.com>
2020-12-18 10:56:53 -0500

    Proyecto api
    se crea controladores y product

---
+++ Sales.Domain/Models/DataContext.cs
@@ -0,0 +1,25 @@
+﻿namespace Sales.Domain.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using Sales.Common.Models;
+
+    public class DataContext:DbContext
+    {
+
+        public DataContext() : base("DefaultConnection")//Se pasa  el parametro  segun el web config de backe, base se utiliza por
+            // habia heredado  de dbcontext
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Sales.Common.Models.Product> Products { get; set; }
+
+        /*
+        public IQueryable<Product> Products()
+        {
+            throw new NotImplementedException();
+        }*/
+    }
+}
